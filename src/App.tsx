0c87a41ebb8b209d927ef87dc99f2d2ad6c2a4c5
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2022-04-30 15:37:43 +0900

    configured styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import StyledKeyCap from "./component/StyledKeyCap"
+import StyledKeyboard from './component/StyledKeyboard';
 
 function App() {
   return (
     <div className="App">
         {/* <Counter /> */}
+        <StyledKeyCap />
+        <StyledKeyboard />
     </div>
   );
 }
---
+++ src/component/StyledKeyboard.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+
+const StyledKeyboard = styled.div`
+    width: 1000px;
+    height: 500px;
+    background-color: lightblue;
+`
+
+export default StyledKeyboard
